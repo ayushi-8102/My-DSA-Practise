705e8ed310b9dbb87451ff8aaf48d1864d60f376
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2023-07-02 05:57:42 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -15,13 +15,10 @@ class Solution {
         int start=0;
         int end=n-1;
         while(start<=end){
-            int mid= (start+end)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==k)return mid;
-            else if(arr[mid]>k)
-            end=(mid-1);
-            else if(arr[mid]<k)
-            start=(mid+1);
-            
+            else if(arr[mid]>k)end=mid-1;
+            else start=mid+1;
         }
         return -1;
     }
