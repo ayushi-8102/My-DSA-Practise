108386f0e8143b3de2b6cf3352db68e244bb3cef
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2023-10-11 19:07:58 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -6,30 +6,26 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-static int dp[501][501];
+
 class Solution{
+    int dp[501][501];
 public:
 int solve(int arr[],int i,int j){
-    
-     int temp ;
-     int mn= INT_MAX;
-    //base condition
-    if(i>=j)
-    return 0;
+    if(i>=j)return 0;
     if(dp[i][j]!=-1)return dp[i][j];
+    int ans=INT_MAX;
     for(int k=i;k<=j-1;k++){
-        temp=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[j]*arr[k]);
-        if(temp<mn)
-        mn=temp;
-          
+        int temp=solve(arr,i,k)+solve(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
+        ans=min(temp,ans);
     }
-    return dp[i][j]=mn;
+    return dp[i][j]=ans;
 }
     int matrixMultiplication(int N, int arr[])
     {
         // code here
         memset(dp,-1,sizeof(dp));
         return solve(arr,1,N-1);
+        
     }
 };
 
