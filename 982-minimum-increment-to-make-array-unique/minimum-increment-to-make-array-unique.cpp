64329fafc7da74fda42c914a4cf420259336b628
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-06-15 02:57:45 +0530

    Time: 114 ms (76.53%) | Memory: 69.2 MB (24.24%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        map<int,int>mp;
+       /* map<int,int>mp;
         for(auto i:nums)mp[i]++;
         int count=0;
         for(auto it:mp){
@@ -12,6 +12,16 @@ public:
         }
 
         }
+    return count;*/
+    //Counting-Sort
+    int count=0;
+    sort(nums.begin(),nums.end());
+    for(int i=1;i<nums.size();i++){
+     if(nums[i]<=nums[i-1]){
+        count+=1+nums[i-1]-nums[i];
+        nums[i]=nums[i-1]+1;
+     }   
+    }
     return count;
     }
 };
