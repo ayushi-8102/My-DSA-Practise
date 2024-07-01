073f5ecdfd5d41cd3bf1470d55b7a09f5e15d994
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-07-02 05:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (8.28%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        for(auto i:arr){
+            if(i%2!=0){count++;
+            if(count==3)return true;}
+          else count =0;
+        }
+        return false;
+    }
+};
