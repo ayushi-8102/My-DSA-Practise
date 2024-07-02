36c01db345c5a1fb2926d7e2c513ae4d6ec84a43
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-07-02 05:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (94.86%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-bool istriangle(vector<int>& nums){
-    if(nums[0]+nums[1]>nums[2]&&nums[1]+nums[2]>nums[0]&&nums[0]+nums[2]>nums[1])return true;
-    return false;
-}
     string triangleType(vector<int>& nums) {
-     if(istriangle(nums)==false)return "none";
-    set<int>st;
-    for(auto i:nums)st.insert(i);
-    if(st.size()==1&&istriangle(nums))return "equilateral";
-    else if(st.size()==2&&istriangle(nums))return "isosceles";
-    else return "scalene";
+        if (!((nums[0] + nums[1] > nums[2]) && (nums[0] + nums[2] > nums[1]) && (nums[2] + nums[1] > nums[0])))
+            return "none";  
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral"; 
+        else if (nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2])
+            return "scalene";
+
+        return "isosceles";
     }
 };
