5a5c3dde1e3ade466c181a7e5d67a7ea5ed2b048
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-07-03 02:55:12 +0530

    Time: 31 ms (98.22%) | Memory: 52.6 MB (99.70%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+Solution(){
+         ios_base::sync_with_stdio(false);
+         cin.tie(NULL);
+         cout.tie(NULL);
+    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i=0, j=0;
         while(i<nums1.size()&&j<nums2.size()){
