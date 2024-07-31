ba273480ad13b1e8050aafc274d0380b0838d9be
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-07-31 23:42:21 +0530

    Time: 5 ms (49.25%) | Memory: 15.3 MB (34.78%) - LeetSync

---
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   int dp[1001][1001];
+    int solve(int i, int remWidth, int maxHeight, int shelfWidth, vector<vector<int>>& books){
+        if(i >= books.size()) return maxHeight;
+
+        if(dp[i][remWidth]!=-1) return dp[i][remWidth];
+
+        int width = books[i][0], height = books[i][1];
+        int keep = INT_MAX, skip = INT_MAX;
+        if(width <= remWidth){
+            keep = solve(i+1, remWidth-width, max(maxHeight, height), shelfWidth, books);
+        }
+        skip = maxHeight + solve(i+1, shelfWidth-width, height, shelfWidth, books);
+
+        return dp[i][remWidth] = min(keep, skip);
+    }
+
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(0, shelfWidth, 0, shelfWidth, books);
+        return ans;
+        
+    }
+};
