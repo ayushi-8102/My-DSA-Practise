8529fc53ef860708c4c8e001907a4010c92fb390
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2024-08-02 02:06:47 +0530

    Time: 8 ms (47.55%) | Memory: 17.5 MB (33.89%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+      int ans=0;
+      for(auto i:details){
+        int c= stoi(i.substr(11, 2));
+         if(c>60)ans++;
+      }
+      return ans;  
+    }
+};
